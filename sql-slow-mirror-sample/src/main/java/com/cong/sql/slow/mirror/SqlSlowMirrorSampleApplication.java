7b06c7e1b408e86293af948be9d082d1a7cdaa2d
ZHUT\liuhuaicong <liuhuaicong@zhut.cn>
2024-04-12 10:29:46 +0800

    feat: 新增注释

--- sql-slow-mirror-sample/src/main/java/com/cong/sql/slow/mirror/SqlSlowMirrorSampleApplication.java
+++ sql-slow-mirror-sample/src/main/java/com/cong/sql/slow/mirror/SqlSlowMirrorSampleApplication.java
@@ -3,6 +3,12 @@ package com.cong.sql.slow.mirror;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * SQL 慢速镜像示例应用程序
+ *
+ * @author cong
+ * @date 2024/04/12
+ */
 @SpringBootApplication
 public class SqlSlowMirrorSampleApplication {
 
