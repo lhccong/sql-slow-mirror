b09199c56a3aca67df4e5d9a2b18b2e8fb5ff394
ZHUT\liuhuaicong <liuhuaicong@zhut.cn>
2024-04-12 10:12:00 +0800

    feat: 新增 mybatis 切面

---
+++ sql-slow-mirror-core/src/main/java/com/cong/sql/slow/mirror/core/SqlAnalysisAspect.java
@@ -0,0 +1,51 @@
+package com.cong.sql.slow.mirror.core;
+
+import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.executor.statement.StatementHandler;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.plugin.Intercepts;
+import org.apache.ibatis.plugin.Invocation;
+import org.apache.ibatis.plugin.Signature;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
+
+import java.sql.Connection;
+import java.util.Properties;
+
+/**
+ * SQL分析方面
+ *
+ * @author cong
+ * @date 2024/04/12
+ */
+@Intercepts({@Signature(
+        type = StatementHandler.class,
+        method = "prepare",
+        args = {Connection.class, Integer.class}
+), @Signature(
+        type = Executor.class,
+        method = "update",
+        args = {MappedStatement.class, Object.class}
+),@Signature(
+        type = Executor.class,
+        method = "query",
+        args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class}
+)})
+public class SqlAnalysisAspect implements Interceptor {
+    @Override
+    public Object intercept(Invocation invocation) throws Throwable {
+        //执行完后放行
+        return invocation.proceed();
+    }
+
+    @Override
+    public Object plugin(Object target) {
+        return Interceptor.super.plugin(target);
+    }
+
+    @Override
+    public void setProperties(Properties properties) {
+        Interceptor.super.setProperties(properties);
+    }
+}
