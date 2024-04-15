c7018360e0863e1daeaebd05cc456e642f06e202
ZHUT\liuhuaicong <liuhuaicong@zhut.cn>
2024-04-15 09:23:46 +0800

    feat: 新增配置注入

--- sql-slow-mirror-sample/src/main/java/com/cong/example/config/MyBatisPlusConfig.java
+++ sql-slow-mirror-sample/src/main/java/com/cong/example/config/MyBatisPlusConfig.java
@@ -1,12 +1,16 @@
 package com.cong.example.config;
 
+import com.cong.sql.slowmirror.config.ConfigUtils;
 import com.cong.sql.slowmirror.core.SqlAnalysisAspect;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Properties;
+
 /**
  * MyBatis Plus 配置
+ *
  * @author <a href="https://github.com/lhccong">...</a>
  */
 @Configuration
@@ -20,7 +24,19 @@ public class MyBatisPlusConfig {
      */
     @Bean
     public SqlAnalysisAspect sqlAnalysisAspect() {
-        return new SqlAnalysisAspect();
+
+        // 加载配置文件，此处加载的是名为"sql.slow.mirror"的配置
+        Properties properties = ConfigUtils.loadConfig("sql.slow.mirror");
+
+        // 创建SQL分析切面的实例
+        SqlAnalysisAspect sqlAnalysisAspect = new SqlAnalysisAspect();
+
+        // 将加载的配置属性设置到SQL分析切面实例中
+        sqlAnalysisAspect.setProperties(properties);
+
+        // 返回配置完毕的SQL分析切面实例
+        return sqlAnalysisAspect;
     }
+
 
 }
