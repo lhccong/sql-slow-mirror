9f5a9de32c3deee80c787f224837d17529310ec9
ZHUT\liuhuaicong <liuhuaicong@zhut.cn>
2024-04-15 09:57:49 +0800

    feat: 新增 SQL 分析结果列字段

---
+++ sql-slow-mirror-core/src/main/java/com/cong/sql/slowmirror/rule/MatchColumn.java
@@ -0,0 +1,34 @@
+package com.cong.sql.slowmirror.rule;
+
+import lombok.Getter;
+
+/**
+ * 匹配列
+ *
+ * @author cong
+ * @date 2024/04/15
+ */
+@Getter
+public enum MatchColumn {
+    SELECT_TYPE("selectType"),
+    TABLE("table"),
+    PARTITIONS("partitions"),
+    TYPE("type"),
+    POSSIBLE_KEYS("possibleKeys"),
+    KEY("key"),
+    KEY_LEN("keyLen"),
+    REF("ref"),
+    ROWS("rows"),
+    FILTERED("filtered"),
+    EXTRA("extra");
+
+    /**
+     * 匹配字段
+     */
+    private String column;
+
+    MatchColumn(String column){
+        this.column = column;
+    }
+
+}
