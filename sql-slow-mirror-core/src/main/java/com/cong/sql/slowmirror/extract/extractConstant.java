9b65844227fc6696ba761572439632639f9de60b
ZHUT\liuhuaicong <liuhuaicong@zhut.cn>
2024-04-15 10:39:44 +0800

    feat: 新增提取常量

---
+++ sql-slow-mirror-core/src/main/java/com/cong/sql/slowmirror/extract/extractConstant.java
@@ -0,0 +1,15 @@
+package com.cong.sql.slowmirror.extract;
+
+/**
+ * 提取常量
+ *
+ * @author cong
+ * @date 2024/04/15
+ */
+public interface extractConstant {
+
+    /**
+     * 委托
+     */
+    String DELEGATE = "delegate";
+}
