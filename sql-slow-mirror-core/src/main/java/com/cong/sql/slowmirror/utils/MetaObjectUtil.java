bd88ec48debff1254ea4f662287de7a18a20dbe3
ZHUT\liuhuaicong <liuhuaicong@zhut.cn>
2024-04-15 10:39:07 +0800

    feat: 新增 MetaObject 包装 mybatis 反射工具类

---
+++ sql-slow-mirror-core/src/main/java/com/cong/sql/slowmirror/utils/MetaObjectUtil.java
@@ -0,0 +1,33 @@
+package com.cong.sql.slowmirror.utils;
+
+import org.apache.ibatis.reflection.DefaultReflectorFactory;
+import org.apache.ibatis.reflection.MetaObject;
+import org.apache.ibatis.reflection.ReflectorFactory;
+import org.apache.ibatis.reflection.factory.DefaultObjectFactory;
+import org.apache.ibatis.reflection.factory.ObjectFactory;
+import org.apache.ibatis.reflection.wrapper.DefaultObjectWrapperFactory;
+import org.apache.ibatis.reflection.wrapper.ObjectWrapperFactory;
+
+/**
+ * MetaObject包装mybatis 反射工具类
+ *
+ * @author cong
+ * @date 2024/04/15
+ */
+public class MetaObjectUtil {
+    private static final ObjectFactory DEFAULT_OBJECT_FACTORY = new DefaultObjectFactory();
+    private static final ObjectWrapperFactory DEFAULT_OBJECT_WRAPPER_FACTORY = new DefaultObjectWrapperFactory();
+    private static final ReflectorFactory DEFAULT_REFLECTOR_FACTORY = new DefaultReflectorFactory();
+
+
+    /**
+     * MetaObject 对象实例化
+     *
+     * @param object 对象
+     * @return {@link MetaObject}
+     */
+    public static MetaObject forObject(Object object) {
+        return MetaObject.forObject(object, DEFAULT_OBJECT_FACTORY, DEFAULT_OBJECT_WRAPPER_FACTORY, DEFAULT_REFLECTOR_FACTORY);
+    }
+
+}
