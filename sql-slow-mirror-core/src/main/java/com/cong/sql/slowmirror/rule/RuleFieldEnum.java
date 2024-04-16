5386b48f8c9e6f411ec1710639961b3787df9dc7
ZHUT\liuhuaicong <liuhuaicong@zhut.cn>
2024-04-16 15:09:10 +0800

    feat: 新增规则字段枚举

---
+++ sql-slow-mirror-core/src/main/java/com/cong/sql/slowmirror/rule/RuleFieldEnum.java
@@ -0,0 +1,45 @@
+package com.cong.sql.slowmirror.rule;
+
+/**
+ * 规则字段枚举
+ * @author yangchao341
+ */
+
+public enum RuleFieldEnum {
+    /**
+     * 条件
+     */
+    CONDITION,
+    /**
+     * 优先权
+     */
+    PRIORITY,
+    /**
+     * 名字
+     */
+    NAME,
+    /**
+     * 描述
+     */
+    DESCRIPTION,
+    /**
+     * 行动
+     */
+    ACTION,
+    /**
+     * 得分
+     */
+    SCORE,
+    /**
+     * 原因
+     */
+    REASON,
+    /**
+     * 建议
+     */
+    SUGGESTION,
+    /**
+     * 是否严格
+     */
+    STRICT
+}
