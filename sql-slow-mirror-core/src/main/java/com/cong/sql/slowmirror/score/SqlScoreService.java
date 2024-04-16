eb0303b1d9af830e4bd7b3a7c65ca60b17f2ed19
ZHUT\liuhuaicong <liuhuaicong@zhut.cn>
2024-04-16 10:03:51 +0800

    feat: 新增 SQL 分数服务接口

---
+++ sql-slow-mirror-core/src/main/java/com/cong/sql/slowmirror/score/SqlScoreService.java
@@ -0,0 +1,14 @@
+package com.cong.sql.slowmirror.score;
+
+import com.cong.sql.slowmirror.analysis.SqlAnalysisResultList;
+
+/**
+ * SQL 分数服务
+ *
+ * @author cong
+ * @date 2024/04/16
+ */
+public interface SqlScoreService {
+
+    SqlScoreResult score(SqlAnalysisResultList sqlAnalysisResultDto);
+}
