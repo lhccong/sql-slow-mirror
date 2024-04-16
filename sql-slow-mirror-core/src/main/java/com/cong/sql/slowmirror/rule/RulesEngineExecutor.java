a4b15182d37ca52bd520a19e393e4d89152a242b
ZHUT\liuhuaicong <liuhuaicong@zhut.cn>
2024-04-16 15:31:14 +0800

    feat: 新增规则引擎执行判断

--- sql-slow-mirror-core/src/main/java/com/cong/sql/slowmirror/rule/RulesEngineExecutor.java
+++ sql-slow-mirror-core/src/main/java/com/cong/sql/slowmirror/rule/RulesEngineExecutor.java
@@ -1,9 +1,13 @@
 package com.cong.sql.slowmirror.rule;
 
+import com.cong.sql.slowmirror.analysis.SqlAnalysisResult;
 import com.cong.sql.slowmirror.score.SqlScoreResultDetail;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.math.NumberUtils;
+import org.jeasy.rules.api.Facts;
 import org.jeasy.rules.api.Rules;
+import org.jeasy.rules.api.RulesEngine;
+import org.jeasy.rules.core.DefaultRulesEngine;
 import org.jeasy.rules.mvel.MVELRule;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,9 +15,7 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
@@ -26,12 +28,14 @@ public class RulesEngineExecutor {
 
     private static final Logger logger = LoggerFactory.getLogger(RulesEngineExecutor.class);
 
+    public static final RulesEngine RULES_ENGINE = new DefaultRulesEngine();
+
     private static Rules rules;
 
     /**
      * 匹配规则-评分结果映射关系
      */
-    private static ConcurrentHashMap<String,SqlScoreResultDetail> scoreMap;
+    private static ConcurrentHashMap<String, SqlScoreResultDetail> scoreMap;
 
     private RulesEngineExecutor() {
         throw new IllegalStateException("Utility class");
@@ -60,40 +64,58 @@ public class RulesEngineExecutor {
         properties.forEach((key, value) -> {
             String[] keyArr = StringUtils.split(key.toString(), ".");
             if (keyArr.length == 2) {
-                //获取规则内容,如果没有则创建一条规则,并将它放入 ret 中
+                //获取规则内容,如果规则命中后续操作将规则名称放入 ret 集合中
                 MVELRule rule = ruleMap.getOrDefault(keyArr[0], new MVELRule().name(keyArr[0]).then("ret.add(\"" + keyArr[0] + "\")"));
                 //获取评分规则,如果没有则重新创建一个评分规则对象
                 SqlScoreResultDetail scoreRule = innerScoreMap.getOrDefault(keyArr[0], new SqlScoreResultDetail());
-                switch (RuleFieldEnum.valueOf(keyArr[1].toUpperCase())){
+                switch (RuleFieldEnum.valueOf(keyArr[1].toUpperCase())) {
                     //如果是条件则加入 easyRule 规则中
-                    case CONDITION: rule.when(value.toString());break;
+                    case CONDITION:
+                        rule.when(value.toString());
+                        break;
                     //如果是操作则加入 easyRule 规则中
-                    case ACTION: rule.then(value.toString());break;
+                    case ACTION:
+                        rule.then(value.toString());
+                        break;
                     //如果是名称则加入 easyRule 规则中
-                    case NAME: rule.name(value.toString());break;
+                    case NAME:
+                        rule.name(value.toString());
+                        break;
                     //如果是描述则加入 easyRule 规则中
-                    case DESCRIPTION: rule.description(value.toString());break;
+                    case DESCRIPTION:
+                        rule.description(value.toString());
+                        break;
                     //如果是优先级则加入 easyRule 规则中
-                    case PRIORITY: rule.priority(NumberUtils.toInt(value.toString()));break;
+                    case PRIORITY:
+                        rule.priority(NumberUtils.toInt(value.toString()));
+                        break;
                     //减分 配置到评分规则对象中
-                    case SCORE: scoreRule.setScoreDeduction(NumberUtils.toInt(value.toString()));break;
+                    case SCORE:
+                        scoreRule.setScoreDeduction(NumberUtils.toInt(value.toString()));
+                        break;
                     //原因 配置到评分规则对象中
-                    case REASON: scoreRule.setReason(value.toString());break;
+                    case REASON:
+                        scoreRule.setReason(value.toString());
+                        break;
                     //建议 配置到评分规则对象中
-                    case SUGGESTION: scoreRule.setSuggestion(value.toString());break;
+                    case SUGGESTION:
+                        scoreRule.setSuggestion(value.toString());
+                        break;
                     //是否严格执行 配置到评分规则对象中
-                    case STRICT: scoreRule.setStrict(Boolean.valueOf(value.toString()));break;
+                    case STRICT:
+                        scoreRule.setStrict(Boolean.valueOf(value.toString()));
+                        break;
                 }
                 //将配置好的规则重新放入 ruleMap 中
                 ruleMap.put(keyArr[0], rule);
                 //将配置好的评分规则重新放入 innerScoreMap 中
-                innerScoreMap.put(keyArr[0],scoreRule);
+                innerScoreMap.put(keyArr[0], scoreRule);
             }
 
         });
         //创建规则列表 将上面配置的规则注册进去
         Rules newRules = new Rules();
-        for(Map.Entry<String,MVELRule> ruleEntry : ruleMap.entrySet()){
+        for (Map.Entry<String, MVELRule> ruleEntry : ruleMap.entrySet()) {
             newRules.register(ruleEntry.getValue());
         }
         //规则创建持久化,后续交由执行时来进行使用
@@ -101,6 +123,31 @@ public class RulesEngineExecutor {
         scoreMap = innerScoreMap;
         logger.info("规则引擎配置文件加载成功");
         return true;
+    }
+
+    public static List<SqlScoreResultDetail> execute(SqlAnalysisResult sqlAnalysisResult) {
+        //评分结果
+        ArrayList<SqlScoreResultDetail> retList = new ArrayList<>();
+        //初始化返回规则
+        ArrayList<String> ret = new ArrayList<>();
+
+        //创建事实
+        Facts facts = new Facts();
+        facts.put("param", sqlAnalysisResult);
+        facts.put("ret", ret);
+
+        // 执行规则
+        RULES_ENGINE.fire(rules, facts);
+
+        //获取执行完成的结果
+        ret = facts.get("ret");
+        for (String ruleName : ret) {
+            if (scoreMap.get(ruleName) != null){
+                retList.add(scoreMap.get(ruleName));
+            }
+        }
+
+        return retList;
     }
 
 }
